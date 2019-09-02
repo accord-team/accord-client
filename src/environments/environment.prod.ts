5433777b5531868be43ca2383bb81f13c7dc91e7
mjezior <mateusz.jeziorek@o2.pl>
2019-09-02 18:55:45 +0200

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  appName: 'Weave',
+  firebaseConfig: {
+    apiKey: 'AIzaSyDagoBoyq78eR_TImcDq-PS1OMrQF36HkQ',
+    authDomain: 'weave-a9434.firebaseapp.com',
+    databaseURL: 'https://weave-a9434.firebaseio.com',
+    projectId: 'weave-a9434',
+    storageBucket: 'weave-a9434.appspot.com',
+    messagingSenderId: '200599062859',
+    appId: '1:200599062859:web:616b855507e93c7e'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  appName: 'Weave',
+  firebaseConfig: {
+    apiKey: 'AIzaSyDagoBoyq78eR_TImcDq-PS1OMrQF36HkQ',
+    authDomain: 'weave-a9434.firebaseapp.com',
+    databaseURL: 'https://weave-a9434.firebaseio.com',
+    projectId: 'weave-a9434',
+    storageBucket: 'weave-a9434.appspot.com',
+    messagingSenderId: '200599062859',
+    appId: '1:200599062859:web:616b855507e93c7e'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
